c461f2196c1ec53cb324567e106f06cfcd5ba181
ShiraHella <buckbeak@Buckbeak.local>
2021-08-06 17:28:59 +0900

    Initial build

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hey there! Am I working?</p>
+        <a href="/" className="btn btn-primary">clickidy click</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
